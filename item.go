e497078bed0953184b27808af67f9e15a6eda208
Branden <brandencolen@gmail.com>
2020-11-04 19:55:15 -0600

    added ItemType constants

--- item.go
+++ item.go
@@ -1,22 +1,40 @@
 package hackernews
 
-// Item - GetItem() response struct.
+// ItemType describes what type of object
+// the Item struct represents
+type ItemType string
+
+const (
+	// ItemTypeJob is a job item
+	ItemTypeJob ItemType = "job"
+	// ItemTypeStory is a story item
+	ItemTypeStory ItemType = "story"
+	// ItemTypeComment is a comment item
+	ItemTypeComment ItemType = "comment"
+	// ItemTypePoll is a poll item
+	ItemTypePoll ItemType = "poll"
+	// ItemTypePollOpt is a poll option item
+	ItemTypePollOpt ItemType = "pollopt"
+)
+
+// Item - GetItem() response struct. Items can
+// be any of the ItemType options.
 type Item struct {
-	ID          int    `json:"id,omitempty"`
-	Deleted     string `json:"deleted,omitempty"`
-	Type        string `json:"type,omitempty"`
-	By          string `json:"by,omitempty"`
-	Time        int64  `json:"time,omitempty"`
-	Text        string `json:"text,omitempty"`
-	Dead        bool   `json:"dead,omitempty"`
-	Parent      int    `json:"parent,omitempty"`
-	Poll        int    `json:"poll,omitempty"`
-	Kids        []int  `json:"kids,omitempty"`
-	URL         string `json:"url,omitempty"`
-	Score       int    `json:"score,omitempty"`
-	Title       string `json:"title,omitempty"`
-	Parts       []int  `json:"parts,omitempty"`
-	Descendants int    `json:"descendants,omitempty"`
+	ID          int      `json:"id"`
+	Deleted     string   `json:"deleted"`
+	Type        ItemType `json:"type"`
+	By          string   `json:"by"`
+	Time        int64    `json:"time"`
+	Text        string   `json:"text"`
+	Dead        bool     `json:"dead"`
+	Parent      int      `json:"parent"`
+	Poll        int      `json:"poll"`
+	Kids        []int    `json:"kids"`
+	URL         string   `json:"url"`
+	Score       int      `json:"score"`
+	Title       string   `json:"title"`
+	Parts       []int    `json:"parts"`
+	Descendants int      `json:"descendants"`
 }
 
 // MaxItem response struct. A single item ID.
--- stories.go
+++ stories.go
@@ -16,26 +16,22 @@ const (
 	StoriesJob
 )
 
-func (s StoryType) String() string {
-	switch s {
-	case StoriesTop:
-		return "top"
-	case StoriesNew:
-		return "new"
-	case StoriesBest:
-		return "best"
-	case StoriesAsk:
-		return "ask"
-	case StoriesShow:
-		return "show"
-	case StoriesJob:
-		return "job"
-	}
-	return ""
-}
-
 // Path returned the hackernews API path
 // for the given StoryType
 func (s StoryType) Path() string {
-	return s.String() + "stories"
+	switch s {
+	case StoriesTop:
+		return "topstories"
+	case StoriesNew:
+		return "newstories"
+	case StoriesBest:
+		return "beststories"
+	case StoriesAsk:
+		return "askstories"
+	case StoriesShow:
+		return "showstories"
+	case StoriesJob:
+		return "jobstories"
+	}
+	return ""
 }
